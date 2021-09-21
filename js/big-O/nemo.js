904ca0ad14ba4fb875472ca15447ea670c037a77
ProgrammingWithJon <programmingwithjon@gmail.com>
2021-09-21 08:55:12 -0500

    update nemo.js in big-O

--- js/big-O/nemo.js
+++ js/big-O/nemo.js
@@ -1,11 +1,18 @@
 const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(100).fill('nemo');
+const xl = new Array(1000).fill('nemo');
+const massive = new Array(100000).fill('nemo');
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found Nemo!');
     }
   }
+  let t1 = performance.now();
+  console.log('Call to find Nemo took ' + (t1-t0) + ' millseconds');
 }
 
 findNemo(nemo);
