4e5217df5992f6210bf2e7fb6ebf0c8589991e23
ProgrammingWithJon <programmingwithjon@gmail.com>
2021-09-21 09:02:56 -0500

    commented out the performance.now() calls

--- js/big-O/nemo.js
+++ js/big-O/nemo.js
@@ -5,14 +5,14 @@ const xl = new Array(1000).fill('nemo');
 const massive = new Array(100000).fill('nemo');
 
 function findNemo(array) {
-  let t0 = performance.now();
+  // let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === 'nemo') {
       console.log('Found Nemo!');
     }
   }
-  let t1 = performance.now();
-  console.log('Call to find Nemo took ' + (t1-t0) + ' millseconds');
+  // let t1 = performance.now();
+  // console.log('Call to find Nemo took ' + (t1-t0) + ' millseconds');
 }
 
 findNemo(nemo);
